97864370d65cf90d7a74e76095957d17a6f6c1a9
Cameron B <cameronwbrown@outlook.com>
2019-05-06 20:20:31 +1000

    old method with depth correction

--- uart_send_test.py
+++ uart_send_test.py
@@ -6,17 +6,11 @@ print('started')
 while True:
 	#x = int(input(">> "))
 	#ser.write(x.to_bytes(1,'little'))
-	ser.flush()
+	#ser.flush()
 	#print('sent')
-<<<<<<< HEAD
-	#print("waiting")
-	#time.sleep(1)
-	#message = ser.read()
-	#print(list(message))
-=======
 	#time.sleep(1)
 	#print("waiting")
 	#ser.read(3)
 	message = ser.read(1)
 	print(list(message))
->>>>>>> a6ddd2b44dc45c2827bfa763b4c4e2a6748a0567
+
